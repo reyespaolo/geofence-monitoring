24e01d9884ea4dfeb793b9d4f84295e11c1895c9
Paolo Reyes <reyes.paolo@360logix.com>
2017-09-27 13:27:48 +0800

    update

--- geofence/index.js
+++ geofence/index.js
@@ -3,6 +3,7 @@ let EventEmitter = require('events').EventEmitter;
 let mongoose = require('mongoose');
 var geofence = require('./geofence.model.js');
 let util = require('util');
+let Polygon = require('polygon');
 const _ = require('lodash');
 mongoose.Promise = global.Promise;
 
@@ -10,12 +11,12 @@ var requiredOptions = [
     'dbURL',
 ];
 
-function geocacherError(message) {
+function geoFenceError(message) {
   Error.captureStackTrace(this, this.constructor);
   this.name = this.constructor.name;
   this.message = (message || '');
 }
-util.inherits(geocacherError, Error);
+util.inherits(geoFenceError, Error);
 
 const validate = options => {
   requiredOptions.forEach(function (option) {
@@ -52,8 +53,7 @@ GeoFence.prototype.start = function () {
 
 
 GeoFence.prototype.lookUpGeofence = function(longitude, latitude, cb){
-  var self = this
-
+  var self = this;
   var coords = {
           type: "Point",
           coordinates: [longitude, latitude]
@@ -82,11 +82,61 @@ GeoFence.prototype.lookUpGeofence = function(longitude, latitude, cb){
 }
 
 GeoFence.prototype.arrangeGeofence = (geofence, cb) => {
-  console.log(geofence)
+  var arrangement = new Array();;
+  var firstRun = true;
+  for(var key in geofence){
+    for(var compareKey in geofence){
+      if(geofence[key].name !== geofence[compareKey].name){
+        var flag = (new Polygon(geofence[key].geofence.coordinates[0]).containsPolygon(new Polygon(geofence[compareKey].geofence.coordinates[0])))
 
+        if(flag){
+          if(firstRun){
+            arrangement.push([geofence[key].name, geofence[compareKey].name])
+            firstRun = false;
+          }else{
+            for(var arrKey in arrangement){
+              // console.log(arrangement[arrKey][0])
+              // console.log(arrangement[arrKey].length)
+              if(geofence[key].name === arrangement[arrKey][0]){
+                console.log(arrangement[arrKey].length)
+                arrangement[arrKey].splice(1, 0, geofence[compareKey].name);
+
+                // arrangement[arrKey][arrangement[arrKey].length] = geofence[compareKey].name
+              }else{
+                // arrangement.push([geofence[key].name, geofence[compareKey].name])
+              }
+            }
+          }
+
+        }
+          // if(flag) console.log(`${flag} geofence ${geofence[key].name} -> ${geofence[compareKey].name}`)
+          // var array = [
+          //   ['1st Dimension' ,"2dimensional"]
+          // ]
+          // arrangement.push(array)
+        // if(flag === true){
+        // }
+        // var firstPolygon = new Polygon(geofence[key].geofence.coordinates);
+        // var comparePolygon = new Polygon(geofence[compareKey]);
+        // console.log(firstPolygon.containsPolygon(comparePolygon))
+        // containsPolygon()
+        // console.log(geofence[key])
+        // console.log(geofence[compareKey])
+        // console.log(geofence[key].geofence.coordinates[0])
+        // console.log(new Polygon(geofence[key].geofence.coordinates[0]))
+        // if(new Polygon(geofence[key].geofence.coordinates[0]).containsPolygon(new Polygon(geofence[compareKey].geofence.coordinates[0]))){
+        //   arrangedGeofence[key].children = {"test":"Test"}
+        //   console.log("++++++++")
+        // }
+        // console.log(geofence[compareKey])
+      }
+    }
+  }
+  // alert(images[0][1]); // b
+
+  console.log(arrangement)
 }
 module.exports = GeoFence;
-
 
 //TEST Point in both 121.00158333778381,14.6328616393338
 //TEST Point for Banawe Area Only 121.00127756595612, 14.634185192472579
